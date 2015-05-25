19bb7adba4747876c09033f5c2f47f8a2c10a488
Bogdan Busuioc <bogdan.busuioc@gmail.com>
2015-05-25 21:26:02 +0300

    ajax

--- src/AppBundle/Resources/public/js/app.js
+++ src/AppBundle/Resources/public/js/app.js
@@ -1,9 +1,24 @@
 $(function () {
     setupUi();
 });
+
 function setupUi() {
     $(':input').addClass('ui-widget-content');
     $('input[type="submit"], .button, #footer a').button();
     $('th').addClass('ui-widget-header');
     $('.tree').menu();
+    $('.show-dialog').click(function () {
+        showDialog(this);
+        return false;
+    });
+}
+
+function showDialog(link) {
+
+    var url = $(link).prop('href');
+    var dialog = $( "#dialog" );
+    dialog.load(url, function () {
+        var dialogW = dialog.dialog({modal: true});
+        dialogW.dialog('open');
+    });
 }
